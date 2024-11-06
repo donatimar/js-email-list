f36545d5d3b12f9db94a93c575a7ee3b2326500c
donatimar <donatimar@gmail.com>
2024-11-06 14:47:05 +0100

    added innerHTML and ul

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 function generateEmails() {
   const emailList = [];
+  const emailListEl = document.getElementById("email-list");
   for (let i = 0; i < 10; i++) {
     fetch("https://flynn.boolean.careers/exercises/api/random/mail")
       .then((response) => response.json())
       .then((data) => {
         emailList.push(data.response);
+
+        emailListEl.innerHTML = emailList
+          .map((email) => `<li>${email}</li>`)
+          .join("");
 
         if (emailList.length === 10) {
           console.log("Tutte le email generate:", emailList);
