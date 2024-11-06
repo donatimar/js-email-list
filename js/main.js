4e3ab17ccba14820c5626d0e7e44f91ec6212e21
donatimar <donatimar@gmail.com>
2024-11-06 14:39:38 +0100

    added ciclo for and fetch API

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+function generateEmails() {
+  const emailList = [];
+  for (let i = 0; i < 10; i++) {
+    fetch("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((response) => response.json())
+      .then((data) => {
+        emailList.push(data.response);
 
+        if (emailList.length === 10) {
+          console.log("Tutte le email generate:", emailList);
+        }
+      });
+  }
+}
+
+generateEmails();
